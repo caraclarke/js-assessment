0eb9c97785940480aaba830d58fea849b865f00a
caraclarke <cara.clarke8@gmail.com>
2016-04-19 18:44:42 -0400

    first two tests passing

--- app/arrays.js
+++ app/arrays.js
@@ -3,11 +3,21 @@ exports = (typeof window === 'undefined') ? global : window;
 exports.arraysAnswers = {
 
   indexOf : function(arr, item) {
-
+    for (var i=0; i < arr.length; i++) {
+      if (arr[i] == item) {
+        return i;
+      }
+    }
+    return -1;
   },
 
   sum : function(arr) {
-
+    var total = 0;
+    
+    for (var i=0; i < arr.length; i++) {
+      total += arr[i];
+    }
+    return total;
   },
 
   remove : function(arr, item) {
