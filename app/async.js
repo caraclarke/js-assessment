bd756b0258473442c13d1b52b092e3b6db45ab38
caraclarke <cara.clarke8@gmail.com>
2016-04-20 18:31:17 -0400

    async working

--- app/async.js
+++ app/async.js
@@ -2,10 +2,23 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.asyncAnswers = {
   async : function(value) {
-
+    var defer = $.Deferred();
+    setTimeout(function() {
+      defer.resolve(value);
+    }, 10);
+    return defer.promise();
   },
 
   manipulateRemoteData : function(url) {
+    var defer = $.Deferred();
 
+    $.ajax(url).then(function(resp) {
+      var people = $.map(resp.people, function(person) {
+        return person.name;
+      });
+      defer.resolve(people.sort());
+    });
+
+    return defer.promise();
   }
 };
