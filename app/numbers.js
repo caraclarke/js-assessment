ceb86da17af5d61268306993d945ee4bebcb086b
caraclarke <cara.clarke8@gmail.com>
2016-04-20 20:12:03 -0400

    strings almost all working (reduceString not perfect) and convertToBinary and multiply(rough) passing

--- app/numbers.js
+++ app/numbers.js
@@ -10,10 +10,16 @@ exports.numbersAnswers = {
   },
 
   convertToBinary: function(num) {
+    var arr = [];
 
+    for (var i = 7; i > -1; i--) {
+      arr.push( num & (1 << i) ? 1 : 0 );
+    }
+
+    return arr.join('');
   },
 
   multiply: function(a, b) {
-
+    return +(a * b).toFixed(4);
   }
 };
--- app/strings.js
+++ app/strings.js
@@ -2,12 +2,49 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.stringsAnswers = {
   reduceString: function(str, amount) {
-
+    var output = '';
+    var characterCount = {};
+    
+      for (var i = 0; i < str.length; i++) {
+        
+        if (typeof characterCount[str[i]] === 'undefined') {
+           characterCount[str[i]] = 1;
+             }  else {
+           characterCount[str[i]] = characterCount[str[i]] + 1;
+         }
+         
+         if (characterCount[str[i]] <= amount) {
+           output += str[i];
+         }
+      }
+    
+    return output;
   },
   wordWrap: function(str, cols) {
+      var words = str.split(' ');
+      var ouput = '';
+      var currentLength = 0;
+      var word;
+      var separator;
 
+      for (var i = 0; i < words.length; i++) {
+       currentLength += words[i].length;
+
+       if (i === 0) {
+         separator = '';
+       } else if (currentLength > cols) {
+         currentLength = words[i].length;
+         separator = '\n';
+       } else {
+         separator = ' ';
+       }
+
+       ouput += separator + words[i];
+      }
+
+      return ouput;
   },
   reverseString: function(str) {
-
+    return str.split('').reverse().join('');
   }
 };
